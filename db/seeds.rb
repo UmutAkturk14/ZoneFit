a147dd0c965ee6bb897e98387cd70a513b1ca4d3
Umut Akturk <umut.akturk14@gmail.com>
2023-06-17 13:19:36 +0300

    Seed file done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,62 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+# TODO: Delete everything
+puts "Deleting Comments..."
+Comment.delete_all
+puts "Deleting Posts..."
+Post.delete_all
+puts "Deleting Recipes..."
+Recipe.delete_all
+puts "Deleting Workouts..."
+Workout.delete_all
+puts "Deleting Users..."
+User.delete_all
+
+
+# TODO: Users
+20.times do |x|
+  puts "Creating User #{x + 1}"
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name ,
+    bio: Faker::Lorem.paragraph,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+  )
+end
+
+puts "Users created successfully"
+# TODO: Workouts
+20.times do |x|
+  puts "Creating Workout #{x + 1}"
+  Workout.create!(
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph,
+    url: Faker::Internet.url,
+    difficulty: ["easy", "intermediate", "advanced"].sample,
+    user: User.all.sample
+  )
+end
+puts "Workouts created successfully"
+# TODO: Recipes
+20.times do |x|
+  puts "Creating Recipe #{x + 1}"
+  Recipe.create!(
+    title: Faker::Food.dish,
+    instructions: Faker::Lorem.paragraph(sentence_count: 4),
+    prep_time: "#{(1..150).to_a.sample}m",
+    difficulty: ["easy", "intermediate", "advanced"].sample,
+    user: User.all.sample
+  )
+end
+puts "Recipes created successfully"
+# TODO: Posts
+20.times do |x|
+  puts "Creating Post #{x + 1}"
+  Post.create!(
+    content: Faker::Lorem.paragraph(sentence_count: 5),
+    user: User.all.sample
+  )
+end
+puts "Recipes created successfully"
+# TODO: Comments
