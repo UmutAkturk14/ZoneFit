5dd92748cb3e3a89541a3b0325b89ea83ac868c6
Umut Akturk <umut.akturk14@gmail.com>
2023-06-21 16:17:50 +0300

    Workout user story, somewhat done.

--- app/javascript/controllers/workout_index_controller.js
+++ app/javascript/controllers/workout_index_controller.js
@@ -5,4 +5,21 @@ export default class extends Controller {
   connect() {
     console.log("Connected to Workouts#Index Controller");
   }
+
+
+  hoverButton(event) {
+    const links = event.currentTarget.querySelectorAll('a');
+    const icons = event.currentTarget.querySelectorAll('i');
+
+    links.forEach((link) => link.classList.remove('d-none'));
+    icons.forEach((icon) => icon.classList.remove('d-none'));classList.remove('d-none');
+  }
+
+  hideButton(event) {
+    const links = event.currentTarget.querySelectorAll('a');
+    const icons = event.currentTarget.querySelectorAll('i');
+
+    links.forEach((link) => link.classList.add('d-none'));
+    icons.forEach((icon) => icon.classList.add('d-none'));
+  }
 }
