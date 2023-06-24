354609517761c0381efabac433433536058e5d2b
Timothy AlAzawie <t.alazawie@yahoo.co.uk>
2023-06-24 14:50:23 +0100

    changed home page slightly so they look like figma

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
+    recipes = Recipe.all
+    @top_recipes = []
+    4.times do |_|
+      sampled_items = recipes.sample
+      @top_recipes << sampled_items
+    end
+    workouts = Workout.all
+    @top_workouts = []
+    4.times do |_|
+      sampled_works = workouts.sample
+      @top_workouts << sampled_works
+    end
   end
 end
