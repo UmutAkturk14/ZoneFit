78c25dffe993443110f867f76c5140237fb5298d
Umut Akturk <umut.akturk14@gmail.com>
2023-06-28 21:24:44 +0300

    Dotenv commented out

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
-require 'dotenv/rails-now'
+# require 'dotenv/rails-now'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
