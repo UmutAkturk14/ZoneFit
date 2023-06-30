f90483a3ca1aebfa8853582eb4152f35858fcf12
Umut Akturk <umut.akturk14@gmail.com>
2023-06-30 22:13:11 +0300

    Added thumbnail/attachment feature to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable
+  has_one_attached :attachment
 
   validates :content, presence: true
   validates :user, presence: true
