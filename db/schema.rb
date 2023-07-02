78ca24a44b8326e8b05af2b33f11805af224d17b
Umut Akturk <umut.akturk14@gmail.com>
2023-07-02 15:22:29 +0300

    working on private chat

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_01_214326) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_chatrooms_on_user_id"
   end
 
@@ -65,7 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_01_214326) do
   create_table "conversations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    
+  end
+
   create_table "favorites", force: :cascade do |t|
     t.string "favoritable_type", null: false
     t.bigint "favoritable_id", null: false
